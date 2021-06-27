dc88ce3e4877c5cfd46e6b4fc17860a29559b00d
nowhackathon <snctest@protonmail.com>
2021-06-27 01:22:27 -0700

    1 file modified

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http
   .createServer(function(req, res) {
-    res.write('Hello, Node.js from the bottom of the ocean!'); //write a response to the client
+    res.write('Hello, Node.js from the tree!'); //write a response to the client
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
